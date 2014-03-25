0de16603a2de709f4188d31cce3865f53f6f9277
Zach Turk <zachturk@gmail.com>
2014-03-24 22:29:31 -0700

    Collision detection with walls + player paddle

--- pong/src/com/zachturk/pong/Pong.java
+++ pong/src/com/zachturk/pong/Pong.java
@@ -11,30 +11,45 @@ import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Pong implements ApplicationListener {
 	public static final String TAG = Pong.class.getName();
 		
-	private Renderer renderer;
-	private Controller controller;
+//	private Renderer renderer;
+//	private Controller controller;
+	
 	private SpriteBatch batch;
 	private OrthographicCamera camera;
+	
+	private Texture paddleTexture;
 	private float paddleHeight = 0;
+	private Rectangle paddleBounds;
+	
+	private Texture ballTexture;
 	private Vector2 ballSpeed;
 	private Vector2 ballPosition;
+	private Rectangle ballBounds;
 	
 	@Override
 	public void create() {		
-//		renderer = new Renderer();
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl10.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
 		batch = new SpriteBatch();
 		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.setProjectionMatrix(camera.combined);
+		
+		paddleTexture = paddle();
+		paddleBounds = new Rectangle(0, 0, 8f, 64f);
+		paddleBounds.x = -Gdx.graphics.getWidth()/2+15;
+		
+		ballTexture = ball();
 		ballSpeed = new Vector2(-1, 0);
 		ballPosition = new Vector2(0, 0);
+		ballBounds = new Rectangle(0, 0, 8f, 8f);
 	}
 
 	@Override
@@ -84,28 +99,45 @@ public class Pong implements ApplicationListener {
 			paddleHeight -= 500f*Gdx.graphics.getDeltaTime();
 		}
 		paddleHeight = MathUtils.clamp(paddleHeight, -Gdx.graphics.getHeight()/2, Gdx.graphics.getHeight()/2 - 64);
+		paddleBounds.y = paddleHeight;
 	}
 	
 	private void updateBall() {
 		ballPosition.x += 300*Gdx.graphics.getDeltaTime()*ballSpeed.x;
 		ballPosition.y += 300*Gdx.graphics.getDeltaTime()*ballSpeed.y;
 		
-		if (ballPosition.x < -Gdx.graphics.getWidth()/2 || ballPosition.x > Gdx.graphics.getWidth()/2) {
+		if (ballBounds.overlaps(paddleBounds)) {
+			ballSpeed.x *= -1;
+			ballPosition.x = paddleBounds.x+8;
+			if (Gdx.input.isKeyPressed(Keys.W)) {
+				ballSpeed.y += 1;
+			} else if (Gdx.input.isKeyPressed(Keys.S)) {
+				ballSpeed.y -= 1;
+			} 
+		}
+		if (ballPosition.x < -Gdx.graphics.getWidth()/2) {
+			//reset
+			ballPosition.set(0, 0);
+			ballSpeed.set(-1, 0);
+		} else if (ballPosition.x > Gdx.graphics.getWidth()/2) {
+			//bounce
 			MathUtils.clamp(ballPosition.x, -Gdx.graphics.getWidth()/2, Gdx.graphics.getWidth()/2);
-			ballSpeed.x = -1*ballSpeed.x;
+			ballSpeed.x *= -1;
 		}
-		if (ballPosition.y < -Gdx.graphics.getWidth()/2 || ballPosition.y > Gdx.graphics.getWidth()/2) {
-			MathUtils.clamp(ballPosition.y, -Gdx.graphics.getWidth()/2, Gdx.graphics.getWidth()/2);
-			ballSpeed.y = -1*ballSpeed.y;
+		if (ballPosition.y < -Gdx.graphics.getHeight()/2 || ballPosition.y > Gdx.graphics.getHeight()/2) {
+			MathUtils.clamp(ballPosition.y, -Gdx.graphics.getHeight()/2, Gdx.graphics.getHeight()/2);
+			ballSpeed.y *= -1;
 		}
+		
+		ballBounds.setPosition(ballPosition);
 	}
 	
 	private void renderPaddle() {
-		batch.draw(paddle(), -Gdx.graphics.getWidth()/2+15, paddleHeight);
+		batch.draw(paddleTexture, -Gdx.graphics.getWidth()/2+15, paddleHeight);
 	}
 	
 	private void renderBall() {
-		batch.draw(ball(), ballPosition.x, ballPosition.y);
+		batch.draw(ballTexture, ballPosition.x, ballPosition.y);
 	}
 	
 	private Texture ball() {
